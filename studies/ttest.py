1ec6d22767d8a8ac10f8a1079a56b2585f5c4142
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2022-12-16 18:17:35 +1100

    remove unused test file

--- studies/ttest.py
+++
@@ -1,6 +0,0 @@
-from gassy.two_body import WaveformGenerator
-
-waveform_generator = WaveformGenerator.from_evol_inital_conditions(
-    m=1, M=10, r=-100, num_periods=0.1, stellar_polytropic_index=1
-)
-waveform_generator.plot(distance=100)
